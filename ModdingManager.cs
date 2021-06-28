d68c04eb1177cadc05acb325f066e60571953073
ComputerElite <230407KH5234@ohg-landau.info>
2021-06-28 18:14:29 +0200

    first upload

---
+++ ModdingManager.cs
@@ -0,0 +1,168 @@
+﻿using ComputerUtils.ADB;
+using ComputerUtils.ConsoleUi;
+using ComputerUtils.FileManaging;
+using ComputerUtils.Logging;
+using Iteedee.ApkReader;
+using QuestPatcher.Axml;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text.Json;
+
+namespace LegacyQuestBSModdingTool
+{
+    // Some of the code is by Lauries QuestPatcher (https://github.com/Lauriethefish/QuestPatcher)
+    public class ModdingManager
+    {
+        public ADBInteractor interactor = new ADBInteractor();
+        public static string apkToolPath = PublicVars.exe + "tools\\apktool.jar";
+        public static string apkSignerPath = PublicVars.exe + "tools\\uber-apk-signer.jar";
+
+        public static string libMain32Path = PublicVars.exe + "tools\\libmain32.so";
+        public static string libMain64Path = PublicVars.exe + "tools\\libmain64.so";
+        public static string libModloader32Path = PublicVars.exe + "tools\\libmodloader32.so";
+        public static string libModloader64Path = PublicVars.exe + "tools\\libmodloader64.so";
+
+        public void StartModding()
+        {
+            Logger.Log("Starting modding of APK");
+            Logger.Log("Checking if required Tools Exist");
+            DownloadFileIfMissing(apkToolPath, "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.5.0.jar");
+            DownloadFileIfMissing(apkSignerPath, "https://github.com/patrickfav/uber-apk-signer/releases/download/v1.2.1/uber-apk-signer-1.2.1.jar");
+
+            DownloadFileIfMissing(libMain32Path, "https://github.com/sc2ad/QuestLoader/releases/download/v1.1.1/libmain32.so");
+            DownloadFileIfMissing(libMain64Path, "https://github.com/sc2ad/QuestLoader/releases/download/v1.1.1/libmain64.so");
+            DownloadFileIfMissing(libModloader32Path, "https://github.com/sc2ad/QuestLoader/releases/download/v1.1.1/libmodloader32.so");
+            DownloadFileIfMissing(libModloader64Path, "https://github.com/sc2ad/QuestLoader/releases/download/v1.1.1/libmodloader64.so");
+            Console.ForegroundColor = ConsoleColor.White;
+            UndefinedEndProgressBar undefinedEndProgressBar = new UndefinedEndProgressBar();
+            string apkPath = PublicVars.exe + "tmpBSApk.apk";
+
+            apkPath = ConsoleUiController.QuestionString("BS APK Path: ").Replace("\"", "");
+            undefinedEndProgressBar.Start();
+            undefinedEndProgressBar.SetupSpinningWheel(500);
+            FileManager.RecreateDirectoryIfExisting(PublicVars.exe + "tmp");
+            undefinedEndProgressBar.UpdateProgress("Pulling APK");
+            /*
+            interactor.adb("pull " + interactor.adbS("shell pm path com.beatgames.beatsaber").Replace("package:", "") + " \"" + apkPath + "\"");
+            if(!File.Exists(apkPath))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                undefinedEndProgressBar.StopSpinningWheel();
+                Logger.Log("APK was unable to get pulled. See above for more info.", LoggingType.Warning);
+                Console.WriteLine("APK was unable to get pulled. Aborting.");
+                return;
+            }
+            */
+
+
+            Logger.Log("Checking if apk is modded");
+            undefinedEndProgressBar.UpdateProgress("Checking if APK is modded", true);
+            ZipArchive apkArchive = ZipFile.Open(apkPath, ZipArchiveMode.Update);
+            if(apkArchive.GetEntry("modded") != null || apkArchive.GetEntry("BMBF.modded") != null)
+            {
+                Logger.Log("APK is already modded");
+                Console.ForegroundColor = ConsoleColor.Green;
+                undefinedEndProgressBar.StopSpinningWheel();
+                Console.WriteLine("APK is already modded. No need to mod");
+                return;
+            }
+            undefinedEndProgressBar.UpdateProgress("Getting APK Version", true);
+            MemoryStream manifestStream = new MemoryStream();
+            apkArchive.GetEntry("AndroidManifest.xml").Open().CopyTo(manifestStream);
+            manifestStream.Position = 0;
+            AxmlElement manifest = AxmlLoader.LoadDocument(manifestStream);
+            string versionName = "";
+            foreach(AxmlAttribute a in manifest.Attributes)
+            {
+                if(a.Name == "versionName")
+                {
+                    Console.WriteLine("\nAPK Version is " + a.Value);
+                    Logger.Log("Apk version is " + a.Value);
+                    versionName = a.Value.ToString();
+                }
+            }
+            bool isApk64Bit = apkArchive.GetEntry("lib/arm64-v8a/libil2cpp.so") != null;
+            undefinedEndProgressBar.UpdateProgress("Adding unstripped libunity to APK if available");
+            string libpath = isApk64Bit ? "lib/arm64-v8a/" : "lib/armeabi-v7a/";
+            if (AttemptDownloadUnstrippedUnity(versionName))
+            {
+                Logger.Log("Adding libunity.so to " + (apkArchive.GetEntry("lib/arm64-v8a/libil2cpp.so") != null ? "lib/arm64-v8a/libunity.so" : "lib/armeabi-v7a/libunity.so"));
+                
+                ZipArchiveEntry unity = apkArchive.GetEntry(libpath + "libunity.so");
+                if (unity != null) unity.Delete();
+                apkArchive.CreateEntryFromFile(PublicVars.exe + "tmp\\libunity.so", libpath + "libunity.so");
+            }
+
+            undefinedEndProgressBar.UpdateProgress("Adding modloader", true);
+            apkArchive.CreateEntryFromFile(isApk64Bit ? libModloader64Path : libModloader32Path, libpath + "libmodloader.so");
+
+            undefinedEndProgressBar.UpdateProgress("Adding libmain", true);
+            ZipArchiveEntry main = apkArchive.GetEntry(libpath + "libmain.so");
+            if (main != null) main.Delete();
+            apkArchive.CreateEntryFromFile(isApk64Bit ? libMain64Path : libMain32Path, libpath + "libmain.so");
+            apkArchive.CreateEntry("modded");
+
+            undefinedEndProgressBar.UpdateProgress("Saving", true);
+            apkArchive.Dispose();
+
+            undefinedEndProgressBar.StopSpinningWheel();
+
+            Console.WriteLine("Modding should be finished now. Please install the apk manually as I want to game now so I didn't add automatic install. It's named tmpBSApk.apk");
+        }
+
+        // Uses https://github.com/Lauriethefish/QuestUnstrippedUnity to download an appropriate unstripped libunity.so for beat saber if there is one
+        private bool AttemptDownloadUnstrippedUnity(string version)
+        {
+            Logger.Log("Checking index for unstrippedUnity");
+            WebClient c = new WebClient();
+            string libUnityIndexString = c.DownloadString("https://raw.githubusercontent.com/Lauriethefish/QuestUnstrippedUnity/main/index.json");
+            Dictionary<string, Dictionary<string, string>> index = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, string>>>(libUnityIndexString);
+            string appId = "com.beatgames.beatsaber";
+            if (index.ContainsKey(appId))
+            {
+                if(index[appId].ContainsKey(version))
+                {
+                    DownloadProgressUI downloadProgressUI = new DownloadProgressUI();
+                    Console.WriteLine();
+                    return downloadProgressUI.StartDownload("https://raw.githubusercontent.com/Lauriethefish/QuestUnstrippedUnity/main/versions/" + index[appId][version] + ".so", PublicVars.exe + "tmp\\libunity.so");
+                } else
+                {
+                    Logger.Log("No unstripped libunity found. It does exist for another version of the app");
+                }
+            } else
+            {
+                Logger.Log("No unstripped libunity found.", LoggingType.Warning);
+            }
+            return false;
+        }
+
+        public void StartJar(string file, string args)
+        {
+            Logger.Log("Starting " + file + " with args " + args);
+            Process p = Process.Start("java.exe", "-jar " + file + " " + args);
+            p.WaitForExit();
+            Logger.Log("Process finished");
+        }
+
+        public void DownloadFileIfMissing(string filePath, string downloadLink)
+        {
+            if(!File.Exists(filePath))
+            {
+                string fileName = Path.GetFileName(filePath);
+                Logger.Log(fileName + " doesn't exist. Downloading");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(fileName + " hasn't been downloaded yet. It'll be downloaded");
+                DownloadProgressUI downloadProgressUI = new DownloadProgressUI();
+                bool success = downloadProgressUI.StartDownload(downloadLink, filePath);
+                Console.WriteLine();
+            } else
+            {
+                Logger.Log(Path.GetFileName(filePath) + " exists. Not downloading");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using ComputerUtils.ConsoleUi;
+using ComputerUtils.FileManaging;
+using ComputerUtils.Logging;
+using ComputerUtils.Updating;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegacyQuestBSModdingTool
+{
+    class Program
+    {
+        public static Updater updater = new Updater("0.1", "N/A", "Legacy Quest Beat Saber Modding Tool");
+        static void Main(string[] args)
+        {
+            Logger.SetLogFile(AppDomain.CurrentDomain.BaseDirectory + "log.log");
+            Logger.LogRaw("\n\n");
+            Logger.Log("Starting " + updater.AppName + " version " + updater.version);
+            SetupExceptionHandlers();
+            if (args.Length == 1 && args[0] == "--update")
+            {
+                Logger.Log("Starting in update mode");
+                updater.Update();
+                return;
+            }
+            Console.WriteLine("Welcome to Legacy Quest Beat Saber Modding Tool. Navigate the menu by typing the corresponding number to the choice you want to do and answering the asked Question. If you get stuck in a loop simply exit the Program and inform ComputerElite.");
+            ModdingMenu m = new ModdingMenu();
+            m.Start();
+        }
+
+        public static void SetupExceptionHandlers()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            HandleExtenption((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                HandleExtenption(e.Exception, "TaskScheduler.UnobservedTaskException");
+                e.SetObserved();
+            };
+        }
+
+        public static void HandleExtenption(Exception e, string source)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Logger.Log("An unhandled exception has occured:\n" + e.ToString(), LoggingType.Crash);
+            Console.WriteLine("\n\nAn unhandled exception has occured. Check the log for more info and send it to ComputerElite for the (probably) bug to get fix. Press any key to close out.");
+            Console.ReadKey();
+            Logger.Log("Exiting cause of unhandled exception.");
+            Environment.Exit(0);
+        }
+    }
+
+    public class ModdingMenu
+    {
+        public ModdingManager moddingManager = new ModdingManager();
+        public void Start()
+        {
+            SetupProgram();
+            while(true)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                switch (ConsoleUiController.ShowMenu(new string[] { "Mod APK", "Install Songs", "Install Mods", "Exit" }))
+                {
+                    case "1":
+                        moddingManager.StartModding();
+                        break;
+                    case "2":
+                        Console.WriteLine("Idiot");
+                        break;
+                    case "3":
+                        Console.WriteLine("I hate you");
+                        break;
+                    case "4":
+                        Logger.Log("Exiting");
+                        System.Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+
+        public void SetupProgram()
+        {
+            if(Program.updater.CheckUpdate())
+            {
+                Logger.Log("Update available. Asking user if they want to update");
+                string choice = ConsoleUiController.QuestionString("Do you want to update? (Y/n): ");
+                if (choice.ToLower() == "y" || choice == "")
+                {
+                    Program.updater.StartUpdate();
+                }
+                Logger.Log("Not updating.");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Creating required directories if not existing");
+            FileManager.CreateDirectoryIfNotExisting(PublicVars.exe + "tools");
+        }
+    }
+
+    public class PublicVars
+    {
+        public static string exe = AppDomain.CurrentDomain.BaseDirectory;
+    }
+}
